a738db6ccfc34c4bd27c21cdb8fe167b29efbb02
Mariana <yuliiamotuliak@gmail.com>
2025-12-09 13:04:55 -0500

    Update ad script with new data-admpid

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -22,7 +22,7 @@ import DemoPreview from "./components/demoPreview";
 
 const Home = () => {
   return (<>
-    <script async src="https://js.onclckmn.com/static/onclicka.js" data-admpid="400437"></script>
+    <script async src="https://js.onclckmn.com/static/onclicka.js" data-admpid="400615"></script>
     <div className="bg-[#f7f7f7] flex justify-center items-center overflow-x-hidden">
       <div className="border-l border-r max-w-4xl w-full mx-auto border-dashed h-full flex justify-center flex-col border-gray-300">
         
