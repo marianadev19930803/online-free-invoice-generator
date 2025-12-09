a58f4912d2e52cd6fafa472f7abe7dcf31c9ad7d
Mariana <yuliiamotuliak@gmail.com>
2025-12-09 15:17:26 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -75,6 +75,23 @@ export default function RootLayout({
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#f97316" />
         <meta name="msapplication-TileColor" content="#f97316" />
         <meta name="theme-color" content="#f97316" />
+        <script
+          type="text/javascript"
+          src="//pl28225690.effectivegatecpm.com/3c/91/79/3c9179bcc776f30dc1a579d80aff60b0.js"
+        ></script>
+        <script type="text/javascript">
+          atOptions = {
+          	'key' : '7597aa772c2c514edb50598d1254e478',
+          	'format' : 'iframe',
+          	'height' : 90,
+          	'width' : 728,
+          	'params' : {}
+          };
+        </script>
+        <script
+          type="text/javascript"
+          src="//www.highperformanceformat.com/7597aa772c2c514edb50598d1254e478/invoke.js"
+        ></script>
       </head>
       <body className={`${GeistSans.className}`}>{children}</body>
     </html>
